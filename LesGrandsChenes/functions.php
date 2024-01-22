005a277c24bb07842af7a9fc1a6ca3833f6d0f59
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2024-01-22 17:14:10 +0100

    reset of the tree almost done, need to review links, and customize emojis

---
+++ LesGrandsChenes/functions.php
@@ -0,0 +1,96 @@
+<?php
+
+
+    add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles(){
+    // Chargement du css/theme.css pour nos personnalisations
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
+
+}
+
+  function lesgrandschenes_add_admin_pages() {
+    add_menu_page(__('Paramètres du thème Les grands chênes', 'lesgrandschenes'), __('lesgrandschenes', 'lesgrandschenes'), 'manage_options', 'lesgrandschenes-settings', 'lesgrandschenes_theme_settings', 'dashicons-admin-settings', 60); 
+  }
+
+  function lesgrandschenes_theme_settings() {
+    echo '<h1>'.esc_html( get_admin_page_title() ).'</h1>';
+    echo '<form action="options.php" method="post" name="lesgrandschenes_settings">';
+    echo '<div>';
+
+    settings_fields('lesgrandschenes_settings_fields');
+    do_settings_sections('lesgrandschenes_settings_section');
+    submit_button();
+
+    echo '</div>';
+    echo '</form>';
+  }
+
+  function lesgrandschenes_settings_register() {
+    register_setting('lesgrandschenes_settings_fields', 'lesgrandschenes_settings_fields', 'lesgrandschenes_settings_fields_validate');
+    add_settings_section('lesgrandschenes_settings_section', __('Paramètres', 'lesgrandschenes'), 'lesgrandschenes_settings_section_introduction', 'lesgrandschenes_settings_section');
+    add_settings_field('lesgrandschenes_settings_field_introduction', __('Introduction', 'lesgrandschenes'), 'lesgrandschenes_settings_field_introduction_output', 'lesgrandschenes_settings_section', 'lesgrandschenes_settings_section');
+    add_settings_field('lesgrandschenes_settings_field_phone_number', __('Numéro de téléphone', 'lesgrandschenes'), 'lesgrandschenes_settings_field_phone_number_output', 'lesgrandschenes_settings_section', 'lesgrandschenes_settings_section');
+    add_settings_field('lesgrandschenes_settings_field_mobile_number', __('Numéro de mobile', 'lesgrandschenes'), 'lesgrandschenes_settings_field_mobile_number_output', 'lesgrandschenes_settings_section', 'lesgrandschenes_settings_section');
+    add_settings_field('lesgrandschenes_settings_field_email', __('Email', 'lesgrandschenes'), 'lesgrandschenes_settings_field_email_output', 'lesgrandschenes_settings_section', 'lesgrandschenes_settings_section');
+    add_settings_field('lesgrandschenes_settings_field_lienresa', __('Lien de reservation', 'lesgrandschenes'), 'lesgrandschenes_settings_field_lienresa_output', 'lesgrandschenes_settings_section', 'lesgrandschenes_settings_section');
+   
+    }
+
+  function lesgrandschenes_settings_section_introduction() {
+    echo __('Paramètrez les différentes options de votre thème lesgrandschenes.', 'lesgrandschenes');
+  }
+
+  function lesgrandschenes_settings_fields_validate($inputs) {  
+    if(isset($_POST) && !empty($_POST)):
+      if(isset($_POST['lesgrandschenes_settings_field_introduction']) && !empty($_POST['lesgrandschenes_settings_field_introduction'])):
+        update_option('lesgrandschenes_settings_field_introduction', $_POST['lesgrandschenes_settings_field_introduction']);
+      endif;
+      if(isset($_POST['lesgrandschenes_settings_field_phone_number']) && !empty($_POST['lesgrandschenes_settings_field_phone_number'])):
+        update_option('lesgrandschenes_settings_field_phone_number', $_POST['lesgrandschenes_settings_field_phone_number']);
+      endif;
+      if(isset($_POST['lesgrandschenes_settings_field_mobile_number']) && !empty($_POST['lesgrandschenes_settings_field_mobile_number'])):
+        update_option('lesgrandschenes_settings_field_mobile_number', $_POST['lesgrandschenes_settings_field_mobile_number']);
+      endif;
+      if(isset($_POST['lesgrandschenes_settings_field_email']) && !empty($_POST['lesgrandschenes_settings_field_email'])):
+        update_option('lesgrandschenes_settings_field_email', $_POST['lesgrandschenes_settings_field_email']);
+      endif;
+      if(isset($_POST['lesgrandschenes_settings_field_lienresa']) && !empty($_POST['lesgrandschenes_settings_field_lienresa'])):
+        update_option('lesgrandschenes_settings_field_lienresa', $_POST['lesgrandschenes_settings_field_lienresa']);
+      endif;
+    endif;
+
+    return $inputs;
+  }
+
+  function lesgrandschenes_settings_field_introduction_output() {
+    $value = get_option('lesgrandschenes_settings_field_introduction');
+
+    echo '<input name="lesgrandschenes_settings_field_introduction" type="text" value="'.$value.'" />';
+  }
+
+  function lesgrandschenes_settings_field_phone_number_output() {
+    $value = get_option('lesgrandschenes_settings_field_phone_number');
+
+    echo '<input name="lesgrandschenes_settings_field_phone_number" type="text" value="'.$value.'" />';
+  }
+  function lesgrandschenes_settings_field_mobile_number_output() {
+    $value = get_option('lesgrandschenes_settings_field_mobile_number');
+
+    echo '<input name="lesgrandschenes_settings_field_mobile_number" type="text" value="'.$value.'" />';
+  }
+
+  function lesgrandschenes_settings_field_email_output() {
+    $value = get_option('lesgrandschenes_settings_field_email');
+
+    echo '<input name="lesgrandschenes_settings_field_email" type="text" value="'.$value.'" />';
+  }
+  function lesgrandschenes_settings_field_lienresa_output() {
+    $value = get_option('lesgrandschenes_settings_field_lienresa');
+
+    echo '<input name="lesgrandschenes_settings_field_lienresa" type="text" value="'.$value.'" />';
+  }
+  
+
+  add_action('admin_menu', 'lesgrandschenes_add_admin_pages', 10);
+  add_action('admin_init', 'lesgrandschenes_settings_register');
+  ?>
---
+++ LesGrandsChenes/header.php
@@ -0,0 +1,307 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title><?php _e('Les Grands Chênes', 'lesgrandschenes'); ?></title>
+    <meta name=description content="Gîte de caractère aux portes du Périgord vert, location de vacance de mai à septembre">
+    <meta name=robots content="index, follow">
+    <meta property=og:title content="Les Grands Chênes, gîte de caractère aux portes du Périgord vert">
+    <meta property=og:description content="Gîte de caractère aux portes du Périgord vert, location de vacance de mai à septembre
+    , près de Ribérac, Périgueux et Bergerac">
+    <meta property=og:url content=https://lesgrandschenes.info/>
+    <meta property=og:type content=website>
+    <meta property=og:site_name content="Les Grands Chênes">
+    <meta http-equiv=Permissions-Policy content="interest-cohort=()">
+    <link rel=stylesheet href="<?php echo get_template_directory_uri(); ?>/assets/css/theme.css">
+    <link rel="preload" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/medium/coverpicmedium.webp" media="(min-width: 768px)">
+    <link rel="preload" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/small/coverpicsmall.webp" media="(max-width: 767px)">
+    <script type=application/ld+json>
+        [
+          {
+            "@context": "https://schema.org",
+            "@type": "LodgingBusiness",
+            "name": "Les Grands Chênes",
+            "description": "Site de présentation du gîte les grands chênes",
+            "image": [
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/coverpicsmall.webp",
+              "width": "400",
+              "height": "300",
+              "description": "main cover picture"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-one-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "lodge living room"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-two-small.web",
+              "width": "400",
+              "height": "300",
+              "description": "lodge kitchen area"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-three-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "one of the lodge bedroom"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-four-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "lodge entrance"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-five-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "another view of the lodge living room"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-six-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "another bedroom of the lodge"
+              }
+            ]
+          },
+          {
+            "@context": "https://schema.org",
+            "@type": "ImageGallery",
+            "name": "Slider d'images",
+            "image": [
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-one-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "lodge living room"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-two-small.web",
+              "width": "400",
+              "height": "300",
+              "description": "lodge kitchen area"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-three-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "one of the lodge bedroom"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-four-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "lodge entrance"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-five-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "another view of the lodge living room"
+              },
+              {
+              "@type": "ImageObject",
+              "url": "./assets/images/small/pic-six-small.webp",
+              "width": "400",
+              "height": "300",
+              "description": "another bedroom of the lodge"
+              }
+            ]
+            },
+            {
+            "@context": "https://schema.org",
+            "@type": "LodgingBusiness",
+            "name": "Les Grands Chênes",
+            "url": "https://lesgrandschenes.info/",
+            "priceRange": "141€ to 172€",
+            "telephone": "+33-6-82-26-87-80",
+            "numberOfRooms": "3",
+            "petsAllowed": "true",
+            "paymentAccepted": [
+              "Cash",
+              "CreditCard"
+              ],
+            "image": "./assets/images/small/coverpicsmall.webp",
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "telephone": "+33-6-82-26-87-80",
+              "contactType": "customer service"
+            },
+            "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "Leynie",
+              "addressLocality": "Festalemps",
+              "addressRegion": "Nouvelle Acquitaine",
+              "postalCode": "24410",
+              "addressCountry": "FRANCE"
+            }
+          }
+        ]
+      
+      </script>
+      <script>
+    function initializeTestimonialsSlider(containerId, prevButtonId, nextButtonId) {
+        fetch('<?php echo get_template_directory_uri(); ?>/assets/data/testimonialslist.json')
+            .then(response => response.json())
+            .then(testimonials => {
+                const testimonialsContainer = document.getElementById(containerId);
+                let currentIndex = 0;
+
+                function showTestimonial(index) {
+                    const testimonial = testimonials[index];
+                    const testimonialItem = document.createElement('div');
+                    testimonialItem.classList.add('testimonial-item');
+                    testimonialItem.innerHTML = `
+                        <div class="testimonial-info">
+                            <p class="testimonial-owner">${testimonial.name}</p>
+                            <p class="testimonial-date">${testimonial.date}</p>
+                            <p class="testimonial-content-title">${testimonial.title}</p>
+                            <p class="testimonial-content">${testimonial.content}</p>
+                        </div>
+                        <div class="testimonial-photo-container">
+                            <img src="<?php echo get_template_directory_uri(); ?>${testimonial.photo}" alt="${testimonial.alt}" class="testimonial-photo" width="400" height="300">
+                        </div>
+                    `;
+
+                    testimonialsContainer.innerHTML = '';
+                    testimonialsContainer.appendChild(testimonialItem);
+                }
+
+                function nextTestimonial() {
+                    currentIndex = (currentIndex + 1) % testimonials.length;
+                    showTestimonial(currentIndex);
+                }
+
+                function prevTestimonial() {
+                    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+                    showTestimonial(currentIndex);
+                }
+
+                showTestimonial(currentIndex);
+                document.getElementById(prevButtonId).addEventListener('click', prevTestimonial);
+                document.getElementById(nextButtonId).addEventListener('click', nextTestimonial);
+            })
+            .catch(error => console.error('Error fetching testimonials:', error));
+    }
+
+  
+    document.addEventListener('DOMContentLoaded', function() {
+        initializeTestimonialsSlider('testimonials-container', 'prevButton', 'nextButton', '<?php echo get_template_directory_uri(); ?>/assets/data/testimonialslist.json');
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        initializeTestimonialsSlider('testimonials-container-header', 'prevButtonHeader', 'nextButtonHeader', '<?php echo get_template_directory_uri(); ?>/assets/data/testimonialslist.json');
+    });
+    </script>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+      <header>
+      <div class="sticky-right">
+      <section id="sticky-testimonials" >
+        <h2  class="testimonial">Livre d'or</h2>
+        <div id="testimonials-container-header" class="testimonials-container"></div>
+        <div class="slider-buttons">
+            <button id="prevButtonHeader">Précédent</button>
+            <button id="nextButtonHeader">Suivant</button>
+        </div>
+    </section>
+    
+    
+    <script>
+        initializeTestimonialsSlider('testimonials-container-header', 'prevButtonHeader', 'nextButtonHeader');
+    </script>
+</body>
+
+   
+    <section>
+        <h2 id="reservation">Tarifs 2024 et réservations</h2>
+        <table class="pricing-table">
+            <thead>
+                <tr>
+                    <th>Mois</th>
+                    <th>À la journée</th>
+                    <th>À la semaine</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>MAI</td>
+                    <td>141€</td>
+                    <td>987€</td>
+                </tr>
+                <tr>
+                    <td>JUIN</td>
+                    <td>172€</td>
+                    <td>1204€</td>
+                </tr>
+                <tr>
+                    <td>JUILLET</td>
+                    <td>247€</td>
+                    <td>1729€</td>
+                </tr>
+                <tr>
+                    <td>AOUT</td>
+                    <td>247€</td>
+                    <td>1729€</td>
+                </tr>
+                <tr>
+                    <td>SEPTEMBRE</td>
+                    <td>172€</td>
+                    <td>1204€</td>
+                </tr>
+            </tbody>
+        </table>
+    <p class="reservation-disponibility">
+        <?php _e('Vous pouvez vérifier nos disponibilités et réserver directement sur le site <a href="https://www.abritel.fr/location-vacances/p582656?adults=2&dateless=true&destType=MARKET&destination=Festalemps%2C%20Saint-Privat-en-P%C3%A9rigord%2C%20Dordogne%2C%20France&expediaPropertyId=27126385&latLong=45.22535%2C0.241037&privacyTrackingState=CAN_NOT_TRACK&pwa_ts=1704968919940&referrerUrl=aHR0cHM6Ly93d3cuYWJyaXRlbC5mci9Ib3RlbC1TZWFyY2g%3D&regionId=3000038576&rfrr=HSR&searchId=2b86b646-14e1-479e-bbfe-b6fd2c2604b5&sort=RECOMMENDED&useRewards=true&userIntent=&x_pwa=1" target="_blank">Abritel</a>.', 'lesgrandschenes'); ?>
+    </p>
+    <div class="button-container">
+        <a href="https://www.abritel.fr/location-vacances/p582656?adults=2&dateless=true&destType=MARKET&destination=Festalemps%2C%20Saint-Privat-en-P%C3%A9rigord%2C%20Dordogne%2C%20France&expediaPropertyId=27126385&latLong=45.22535%2C0.241037&privacyTrackingState=CAN_NOT_TRACK&pwa_ts=1704968919940&referrerUrl=aHR0cHM6Ly93d3cuYWJyaXRlbC5mci9Ib3RlbC1TZWFyY2g%3D&regionId=3000038576&rfrr=HSR&searchId=2b86b646-14e1-479e-bbfe-b6fd2c2604b5&sort=RECOMMENDED&useRewards=true&userIntent=&x_pwa=1" target="_blank" class="reserve-button">Je réserve</a>   
+    </div>
+    </section>
+      </div>
+        <div class="container">
+            <div class="header">
+                <a href="/" class="logolink">
+                <img class="logo" width="150" height="150" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.webp" alt="<?php _e('Les Grands Chênes', 'lesgrandschenes'); ?>" />
+                <span >
+                    <h1 class="intro">
+                    <?php 
+                    $introduction = get_option('lesgrandschenes_settings_field_introduction');
+                    
+                    echo $introduction;
+                    ?>
+                    </h1>
+                </span>
+                </a>
+                <ul class="nav">
+                <li class="nav-item"><a href="/" class="nav-link"><?php _e('Accueil', 'lesgrandschenes'); ?></a></li>
+                <li class="nav-item"><a href="#descriptif" class="nav-link"><?php _e('Présentation', 'lesgrandschenes'); ?></a></li>
+                <li class="nav-item"><a href="#situation" class="nav-link"><?php _e('Localisation', 'lesgrandschenes'); ?></a></li>
+                <li class="nav-item"><a href="#equipments" class="nav-link"><?php _e('Equipements', 'lesgrandschenes'); ?></a></li>
+                <li class="nav-item"><a href="#testimonial" class="nav-link"><?php _e('Testimonial', 'lesgrandschenes'); ?></a></li>
+                <li class="nav-item"><a href="#reservation" class="nav-link"><?php _e('Reservation', 'lesgrandschenes'); ?></a></li>
+                </ul>
+            </div>
+            <p class="lesgrandschenes"><?php _e('LES GRANDS CHÊNES', 'lesgrandschenes'); ?></p>
+            <p class="ranks">Classement préfécture ★★★★</p>
+            <p class="ranks">Hébergement Qualité Périgord Vert</p>
+        </div>
+      </header>
